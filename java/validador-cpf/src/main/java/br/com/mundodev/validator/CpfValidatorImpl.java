f0d8e4b8cc120e7ca0314734ddf3188ec39d4edb
Iago Reis <iagoidrs@gmail.com>
2025-06-15 23:30:51 -0300

    refactor: refatorado codigo para reduzir a quantidade de loops de 3 para 1

--- java/validador-cpf/src/main/java/br/com/mundodev/validator/CpfValidatorImpl.java
+++ java/validador-cpf/src/main/java/br/com/mundodev/validator/CpfValidatorImpl.java
@@ -1,5 +1,7 @@
 package br.com.mundodev.validator;
 
+import br.com.mundodev.CpfInvalidoException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -12,49 +14,72 @@ public class CpfValidatorImpl implements CpfValidator {
 
     private static final int PRODUTO_FIXO_CALCULO_DIGITO = 11;
 
-    private static final List<String> INVALID_CPFS = Arrays.asList("00000000000", "11111111111", "22222222222",
-            "33333333333", "44444444444", "55555555555", "66666666666", "77777777777", "88888888888", "99999999999");
+    private static final List<String> INVALID_CPFS = Arrays.asList(
+        "00000000000",
+        "11111111111",
+        "22222222222",
+        "33333333333",
+        "44444444444",
+        "55555555555",
+        "66666666666",
+        "77777777777",
+        "88888888888",
+        "99999999999"
+    );
 
     @Override
     public boolean validate(final String cpf) {
 
-        if (Objects.isNull(cpf) || cpf.trim().length() != 11 || !isCpfWithDigitsOnly(cpf) || INVALID_CPFS.contains(cpf)) {
-            return false;
-        }
-
-        final var cpfSemDigito = cpf.substring(0, 9);
-        final var digito = cpf.substring(9, 11);
-
-        final String primeiroDigito = calcularDigitoUnitario(cpfSemDigito, PRODUTO_CALCULO_PRIMEIRO_DIGITO);
-        final String segundoDigito = calcularDigitoUnitario(cpfSemDigito.concat(primeiroDigito), PRODUTO_CALCULO_SEGUNDO_DIGITO);
-
-        return digito.equals(primeiroDigito.concat(segundoDigito));
-    }
-
-    private boolean isCpfWithDigitsOnly(final String cpf) {
-
-        for (char c : cpf.toCharArray()) {
-            if (!Character.isDigit(c)) {
+        try {
+            if (Objects.isNull(cpf) || cpf.length() != 11 || INVALID_CPFS.contains(cpf)) {
                 return false;
             }
-        }
 
-        return true;
+            final var cpfSemDigito = cpf.substring(0, 9);
+            final var digito = cpf.substring(9, 11);
+
+            final String digitoCalculado = calcularDigito(cpfSemDigito);
+
+            return digito.equals(digitoCalculado);
+        }
+        catch (final CpfInvalidoException e) {
+            return false;
+        }
     }
 
-    private String calcularDigitoUnitario(final String cpf, int produto) {
+    private String calcularDigito(final String cpf) {
 
-        int calculo = 0;
+        int calculoPrimeiroDigito = 0;
+        int calculoSegundoDigito = 0;
+
+        int contador = 0;
 
         for (char c : cpf.toCharArray()) {
-            calculo += Integer.parseInt(String.valueOf(c)) * produto--;
+
+            if (!Character.isDigit(c)) {
+                throw new CpfInvalidoException();
+            }
+
+            calculoPrimeiroDigito += Integer.parseInt(String.valueOf(c)) * (PRODUTO_CALCULO_PRIMEIRO_DIGITO - contador);
+            calculoSegundoDigito += Integer.parseInt(String.valueOf(c)) * (PRODUTO_CALCULO_SEGUNDO_DIGITO - contador);
+
+            contador++;
         }
+
+        final var primeiroDigitoCalculado = tratarCalculoDigito(calculoPrimeiroDigito);
+
+        calculoSegundoDigito += primeiroDigitoCalculado * (PRODUTO_CALCULO_SEGUNDO_DIGITO - contador);
+
+        final var segundoDigitoCalculado = tratarCalculoDigito(calculoSegundoDigito);
+
+        return String.format("%d%d", primeiroDigitoCalculado, segundoDigitoCalculado);
+    }
+
+    private int tratarCalculoDigito(final int calculo) {
 
         final var resto = PRODUTO_FIXO_CALCULO_DIGITO - (calculo % PRODUTO_FIXO_CALCULO_DIGITO);
 
-        final var digitoUnitario = resto < 10 ? resto : 0;
-
-        return String.valueOf(digitoUnitario);
+        return resto < 10 ? resto : 0;
     }
 
 }
